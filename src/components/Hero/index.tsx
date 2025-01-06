74375cd95167c042266da534fbf71590233e3c46
Vinay Tanwar <tanwarv96@gmail.com>
2025-01-06 23:38:14 +0530

    fix slow loading

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Movie } from "@/ts/interfaces";
 
-import { use, useState } from "react";
+import { use, useState, useTransition } from "react";
 import useInterval from "@/hooks/useInterval";
 const PlayIcon = dynamic(() => import("@/icons/PlayArrow"), { ssr: false });
 const InfoCircle = dynamic(() => import("@/icons/InfoCircle"), { ssr: false });
@@ -21,33 +21,45 @@ const Hero = (props: Props) => {
   const results = res.results;
 
   const [currIdx, setCurrIdx] = useState(0);
+  const [isPending, startTransition] = useTransition();
+
   useInterval(5000, () => {
-    setCurrIdx((prev) => {
-      if (prev + 1 === results.length) {
-        return 0;
-      }
-      return prev + 1;
-    });
+    if (!isPending) {
+      startTransition(() => {
+        setCurrIdx((prev) => {
+          if (prev + 1 === results.length) {
+            return 0;
+          }
+          return prev + 1;
+        });
+      });
+    }
   });
   const currMovie = results[currIdx];
+
   return (
     <div className="hero relative h-5/6 shrink-0 mt-4">
       <div className="relative h-[90%] overflow-hidden rounded-[2rem]">
         <div className="flex flex-nowrap overflow-hidden">
-          <Image
-            src={`${IMAGE_BASE_URL}original${currMovie.backdrop_path}`}
-            sizes="100vw"
-            style={{
-              // width: "100%",
-              // height: "auto",
-              objectFit: "cover",
-              objectPosition: "top",
-            }}
-            fill
-            // width={500}
-            // height={334}
-            alt=""
-          />
+          {results.map((result, idx) => {
+            return (
+              <Image
+                key={result.id}
+                className={`rounded-[2rem] ${idx === currIdx ? "" : "hidden"}`}
+                src={`${IMAGE_BASE_URL}original${result.backdrop_path}`}
+                sizes="100vw"
+                style={{
+                  // width: "100%",
+                  // height: "auto",
+                  objectFit: "cover",
+                  objectPosition: "top",
+                }}
+                fill
+                alt={result.name || result.title}
+                priority
+              />
+            );
+          })}
         </div>
         <div
           className="absolute h-9 w-9  border-transparent rounded-br-full  bottom-0 left-cl"
